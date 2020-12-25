2754274903c2f0a924d9475ee1981cc56ca60008
kishan <kishanpatel11999@gmail.com>
2020-12-25 23:53:16 +0530

    a

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/mat-tab/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/mat-table/index.html'));
 });
 // Heroku port
 app.listen(process.env.PORT || 8080);
